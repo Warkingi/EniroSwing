f7e14b120b2274832f0c5a61e65bb186019d7748
Fredrik Sander <fredde.sander@gmail.com>
2016-04-19 05:53:23 +0100

    Loads data from file

--- src/inlupp2/LoadPlacesListener.java
+++ src/inlupp2/LoadPlacesListener.java
@@ -2,7 +2,14 @@ package inlupp2;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 import javax.swing.JFileChooser;
 
@@ -17,12 +24,45 @@ public class LoadPlacesListener implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
+		loadPlaces();
+	}
+	
+	private void loadPlaces() {
 		int answer = fileChooser.showOpenDialog(null);
 		
 		if (answer == JFileChooser.APPROVE_OPTION) {
-			File fil = fileChooser.getSelectedFile();
-			//Load places
+			File file = fileChooser.getSelectedFile();
+			createPlaces(file.getAbsolutePath());
 		}
+	}
+	
+	private List<String> createPlaces(String path) {
+		try {
+			FileReader fr = new FileReader(path);
+			BufferedReader br = new BufferedReader(fr);
+			
+			String line;
+			
+			while ((line=br.readLine()) != null) {
+				String[] data = line.split(",");
+				
+				String type = data[0];
+				String category = data[1];
+				int x = Integer.parseInt(data[2]);
+				int y = Integer.parseInt(data[3]);
+				String name = data[4];
+			}
+			
+			br.close();
+			fr.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
 	}
 
 }
