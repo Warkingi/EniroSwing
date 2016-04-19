85930324c68688f17952d677f2b20d4ad02c3b87
Fredrik Sander <fredde.sander@gmail.com>
2016-04-19 04:00:28 +0100

    Overriding equals and hashcode

--- src/inlupp2/Position.java
+++ src/inlupp2/Position.java
@@ -8,6 +8,22 @@ public class Position {
 		this.y = y;
 	}
 	
+	@Override
+	public boolean equals(Object other) {
+		if (other instanceof Position) {
+			Position p = (Position) other;
+			
+			return getX() == p.getX() && getY() == p.getY();
+		}
+		
+		return false;
+	}
+	
+	@Override
+	public int hashCode() {
+		return getX() * 100000 + getY();
+	}
+	
 	public int getX() { return x; }
 	public int getY() { return y; }
 	public void setX(int x) { this.x = x; }
